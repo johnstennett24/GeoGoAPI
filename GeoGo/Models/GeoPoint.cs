4cee720490a92e8d4efca2f6d0a59f38246d9fa2
johnstennett24 <93450112+johnstennett24@users.noreply.github.com>
2023-02-22 21:01:38 -0700

    continued working on models and services

--- GeoGo/Models/GeoPoint.cs
+++ GeoGo/Models/GeoPoint.cs
@@ -2,6 +2,7 @@ namespace GeoGo.Models;
 
 public class GeoPoint
 {
+  public int Id { get; set; }
   public long lat { get; set; }
   public long lon { get; set; }
   public string message { get; set; }
--- GeoGo/Services/GeoPointServices.cs
+++ GeoGo/Services/GeoPointServices.cs
@@ -1,6 +1,23 @@
+using GeoGo.Models;
+
 namespace GeoGo.Services;
 
 public class GeoPointServices
 {
-  
+  private static List<GeoPoint>? _points = new List<GeoPoint>
+  {
+    new GeoPoint {lat = 0, lon = 0, message = ""}
+  };
+
+  private static int _nextId = 2;
+
+  public static List<GeoPoint> GetAll => _points;
+
+  public static GeoPoint? Get(int id) => _points?.FirstOrDefault(geo => geo.Id == id);
+
+  public static void Add(GeoPoint geoPoint)
+  {
+    geoPoint.Id = _nextId++;
+    _points?.Add(geoPoint);
+  }
 }
